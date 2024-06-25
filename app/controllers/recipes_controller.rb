5f485cd485498eaa93b76ed3d8b529840fe94ba0
Sim <simran.dhaliwal@graze.com>
2024-06-25 16:57:16 +0100

    added matching ingredient count ratio logic

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,25 @@
 class RecipesController < ApplicationController
   def index
     if params[:ingredients]
-      ingredient_names = params[:ingredients].split(',').map(&:strip)
-      ingredients = Ingredient.where(name: ingredient_names)
+      ingredient_keywords = params[:ingredients].split(',').map(&:strip)
+      ingredients = Ingredient.where('name ILIKE ANY (array[?])', ingredient_keywords.map { |kw| "%#{kw}%" })
 
-      if ingredients.size == ingredient_names.size
-        @recipes = Recipe.joins(:ingredients).where(ingredients: { id: ingredients }).group('recipes.id').having('COUNT(recipes.id) = ?', ingredients.size)
+      if ingredients.any?
+        @recipes = Recipe
+                    .select('recipes.*, COUNT(recipe_ingredients.id) as matching_ingredients_count')
+                    .joins(:ingredients)
+                    .where(ingredients: { id: ingredients })
+                    .group('recipes.id')
+                    .order('matching_ingredients_count DESC')
+                    .limit(20)
+                    .map do |recipe|
+                      total_ingredients_count = recipe.ingredients.size
+                      matching_fraction = "#{recipe.matching_ingredients_count}/#{total_ingredients_count}"
+                      matching_ratio = recipe.matching_ingredients_count.to_f / total_ingredients_count
+                      recipe.define_singleton_method(:matching_fraction) { matching_fraction }
+                      recipe.define_singleton_method(:matching_ratio) { matching_ratio }
+                      recipe
+                    end.sort_by { |recipe| -recipe.matching_ratio }
       else
         @recipes = []
       end
